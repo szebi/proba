06f2bb803d1144bc74f8b89832c578531332eb76
Szeberenyi Imre <szebi@iit.bme.hu>
2021-05-07 13:46:30 +0200

    v0

--- welcome/views.py
+++ welcome/views.py
@@ -23,6 +23,3 @@ def health(request):
     """Takes an request as a parameter and gives the count of pageview objects as reponse"""
     return HttpResponse(PageView.objects.count())
 
-
-def hello(request):
-    return HttpResponse("Hello, world. Hello, Django.")
